88fb6dbe61b09a345b525027ca8ed2854e86b530
Emanuele Locambio <emanuele.locambio@itstalentfactory.onmicrosoft.com>
2020-07-15 14:12:22 +0200

    divisione test e aggiunta testcase

--- LeapYear/LeapService.cs
+++ LeapYear/LeapService.cs
@@ -4,9 +4,29 @@ namespace LeapYear
 {
     public class LeapService
     {
-        public static bool IsEven(int number)
+        public static bool IsLeap(int number)
         {
-            return number % 4 == 0 && number % 100 != 0 || number % 400 == 0;
+            return number % 4 == 0;
+        }
+
+        public static bool IsSecolar(int number)
+        {
+            return number % 100 != 0 || number % 400 == 0;
+        }
+
+        public static bool IsGiulian(int number)
+        {
+            return number % 4 == 0 && number <= 1581;
+        }
+
+        public static bool IsBeforeZero(int number)
+        {
+            return number < 0;
+        }
+
+        public static bool IsZero(int number)
+        {
+            return number == 0;
         }
     }
 }
--- LeapYearTest/LeapTest.cs
+++ LeapYearTest/LeapTest.cs
@@ -18,14 +18,52 @@ namespace LeapYearTest
         //[TestCase(2002)]
         #endregion
 
-        [TestCase(400)]
+        [TestCase(2000)]
         [TestCase(2004)]
         [TestCase(2008)]
         [TestCase(108)]
         public void ShouldReturnTrue_WhenYearIsDivisibleByFour(int number)
         {
-            bool isEven = LeapService.IsEven(number);
-            Assert.That(isEven, Is.True);
+            bool isLeap = LeapService.IsLeap(number);
+            Assert.That(isLeap, Is.True);
+        }
+
+        [TestCase(2000)]
+        public void ShouldReturnTrue_WhenYearIsSecolar(int number)
+        {
+            bool isSecolar = LeapService.IsSecolar(number);
+            Assert.That(isSecolar, Is.True);
+        }
+
+        [TestCase(400)]
+        [TestCase(1484)]
+        [TestCase(980)]
+        [TestCase(120)]
+        [TestCase(1268)]
+        public void ShouldReturnTrue_WhenYearIsInGiulianCalendar(int number)
+        {
+            bool isGiulian = LeapService.IsGiulian(number);
+            Assert.That(isGiulian, Is.True);
+        }
+
+        [TestCase(-1978)]
+        [TestCase(-5367)]
+        [TestCase(-946)]
+        [TestCase(-20)]
+        [TestCase(-500)]
+        public void ShouldReturnTrue_WhenYearIsBeforeZero(int number)
+        {
+            bool isBeforeZero = LeapService.IsBeforeZero(number);
+            Assert.That(isBeforeZero, Is.True);
+        }
+
+        [TestCase(0)]
+        //[TestCase(500)]
+        //[TestCase(-500)]
+        public void ShouldReturnTrue_WhenYearIsZero(int number)
+        {
+            bool isZero = LeapService.IsZero(number);
+            Assert.That(isZero, Is.True);
         }
     }
 }
