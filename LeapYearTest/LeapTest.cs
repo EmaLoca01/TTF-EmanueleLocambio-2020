564e114bae50a149ab2d45dc4d91f66b8eda8771
Emanuele Locambio <emanuele.locambio@itstalentfactory.onmicrosoft.com>
2020-07-15 11:50:14 +0200

    test definitivo anni multipli per 4

--- LeapYearTest/LeapTest.cs
+++ LeapYearTest/LeapTest.cs
@@ -11,6 +11,10 @@ namespace LeapYearTest
         }
 
         [TestCase(2000)]
+        [TestCase(2002)]
+        [TestCase(2004)]
+        [TestCase(2006)]
+        [TestCase(2008)]
         public void IsEven_ShouldReturnTrue_WhenYearIsDivisibleByFour(int number)
         {
             bool isEven = LeapService.IsEven(number);
